14480702fd115bb1b94377281e44e0be01d761b7
BachLe27 <bachlamsung123@gmail.com>
2024-02-22 09:17:42 +0700

    update home page

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,36 +2,39 @@ import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 import Image from "next/image"
 import Link from "next/link"
-import lemoLogo from '../../../../../public/lemo-logo.png'
+import bgImage from '../../../../../public/nen.jpg'
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <div className="flex items-center flex-col justify-center">
-          <Image 
-            width={300}
-            height={300}
-            src={lemoLogo}
-            alt="logo"
-            style={{
-              borderRadius: '40%'
-            }}
-          />
-          <Heading
-            level="h2"
-            className="text-xl leading-10 text-ui-fg-subtle font-normal text-amber-600"
-          >
-            Cùng nhau tìm kiếm quà tặng len móc!
-          </Heading>
+      <div className="absolute inset-0 z-10 w-full flex flex-row items-center small:p-32 gap-6">
+
+        <div className="flex flex-col gap-5 w-1/2">
+          <div className="flex flex-col gap-2">
+            <Heading
+              level="h1"
+              className="text-5xl text-amber-500 leading-10 font-bold uppercase"
+              >
+              Lemo - Always by your side 
+            </Heading>
+            <p className="text-lg">
+              Quà tặng handmade bằng len móc tại Hoà Lạc
+            </p>
+          </div>
+        
+          
+            <Button variant="primary" className="bg-amber-500 px-10 text-lg rounded-lg hover:bg-amber-400 text-black">
+              <Link
+                href="/store"
+              >
+                Mua ngay
+              </Link> 
+            </Button>
         </div>
-        <Link
-          href="/store"
-        >
-          <Button variant="primary" className="bg-amber-500 outline-none border-0 hover:bg-amber-400 text-slate-50">
-            Mua ngay
-          </Button>
-        </Link>
+
+        <div className="flex justify-center w-1/2">
+          <Image src={bgImage} alt="bg" width={700} height={500}/>
+        </div>
       </div>
     </div>
   )
